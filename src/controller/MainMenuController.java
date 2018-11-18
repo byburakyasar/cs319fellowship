f67d5100337a1fd125be9fe29016939be76f4b3c
Hikmet <hikmet5548@gmail.com>
2018-11-18 14:24:52 +0300

    levels and gitignore update

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -49,6 +49,15 @@ public class MainMenuController {
     }
 
     @FXML
+    public void levelSelectBtnClicked() throws IOException{
+        Stage current = (Stage) levelSelectBtn.getScene().getWindow();
+        BorderPane root = FXMLLoader.load(getClass().getResource("../view/LevelSelectStage.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+
+        current.setScene(scene);
+    }
+
+    @FXML
     public void exit() {
         Platform.exit();
         System.exit(0);
